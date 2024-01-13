e374c01ab18654e01f14e2c6d63c85f3e25efb83
MartinMurithi <wachiramartin84@gmail.com>
2024-01-13 23:55:03 +0300

    ..

--- src/components/gallery-preview/GalleryPreview.jsx
+++ src/components/gallery-preview/GalleryPreview.jsx
@@ -1,19 +1,57 @@
-import React from "react";
+import React, { useState, useEffect} from "react";
 import { useNavigate } from "react-router-dom";
 import imageList from "../../data/Images";
 import "./GalleryPreview.css";
 import LazyImage from "../LazyImage";
+import LightBox from "../light-box/LightBox";
 
 function GalleryPreview() {
   const navigate = useNavigate();
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+  const [clickedImgPath, setClickedImgPath] = useState(null);
+  const [imgIndex, setImgIndex] = useState(null);
+
+  const onHandleOpenDialog = (index) => {
+    setIsDialogOpen(true);
+    setClickedImgPath(imageList[index].path);
+    setImgIndex(index);
+  };
+
+  const onHandleCloseDialog = () => {
+    setIsDialogOpen(false);
+  };
+
+  const handleNextImage = () => {
+    if (imgIndex < imageList.length - 1) {
+      setImgIndex((imgIndex) => imgIndex + 1);
+    }
+  };
+
+  const handlePrevImg = () => {
+    if (imgIndex > 0) {
+      setImgIndex((imgIndex) => imgIndex - 1);
+    }
+  };
+
+  useEffect(() => {
+    setClickedImgPath(imageList[imgIndex]?.path);
+    console.log(imgIndex);
+  }, [imgIndex, imageList]);
   return (
     <div className="galleryPreview">
       <h3 className="latestWork">My Latest Work</h3>
-      <div className="imgGrid">
+      <div
+        className="imgGrid"
+        style={{
+          display: isDialogOpen ? "none" : "block",
+          marginTop: "20px",
+          width: "98%",
+        }}
+      >
         {/* Display 10 images for the galley preview then navigate them to the gallery page */}
         {imageList?.slice(0, 10)?.map((img, index) => {
           return (
-            <div key={index}>
+            <div key={index} onClick={() => onHandleOpenDialog(index)}>
               <LazyImage
                 src={img.path}
                 alt={img.alt}
@@ -27,6 +65,17 @@ function GalleryPreview() {
       <button className="viewMoreBtn" onClick={() => navigate("/gallery")}>
         View More
       </button>
+      {isDialogOpen && (
+        <LightBox
+          onClose={onHandleCloseDialog}
+          src={clickedImgPath}
+          alt={clickedImgPath}
+          onClickPrev={handlePrevImg}
+          onClickNext={handleNextImage}
+          currentImgIndex={imgIndex + 1}
+          totalImages={imageList.length}
+        />
+      )}
     </div>
   );
 }
