c41ff55ffd76f2793de9955c0e3ff62336da5985
MartinMurithi <wachiramartin84@gmail.com>
2024-01-16 13:03:34 +0300

    ..

--- src/components/light-box/LightBox.jsx
+++ src/components/light-box/LightBox.jsx
@@ -12,21 +12,13 @@ function LightBox({
 }) {
   return (
     <div className="lightBoxBackdrop">
-      <div className="counter-closeBtn">      <p className="imageCounter">{currentImgIndex + "/" + totalImages}</p>
-
-      <div className="closeBtn" onClick={onClose}>
-        X
+      <div className="counter-closeBtn">
+        <p className="imageCounter">{currentImgIndex + "/" + totalImages}</p>
+        <div className="closeBtn" onClick={onClose}>
+          X
+        </div>
       </div>
-</div>
 
-      <img
-        src="/assets/icons8-less-than-50.png"
-        alt="Less Than Icon"
-        width="45px"
-        height="45px"
-        className="prevBtn"
-        onClick={onClickPrev}
-      />
       <div className="lightBox">
         <div className="modalContent">
           <img
@@ -38,14 +30,24 @@ function LightBox({
           />
         </div>
       </div>
-      <img
-        src="/assets/icons8-greater-than-50.png"
-        alt="Greater Than Icon"
-        width="45px"
-        height="45px"
-        className="nextBtn"
-        onClick={onClickNext}
-      />
+      <div className="prev-next-btns">
+        <img
+          src="/assets/icons8-less-than-50.png"
+          alt="Less Than Icon"
+          width="45px"
+          height="45px"
+          className="prevBtn"
+          onClick={onClickPrev}
+        />
+        <img
+          src="/assets/icons8-greater-than-50.png"
+          alt="Greater Than Icon"
+          width="45px"
+          height="45px"
+          className="nextBtn"
+          onClick={onClickNext}
+        />
+      </div>
     </div>
   );
 }
